dbcecacd59a218d35d54e80719693efa0d053ba7
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2020-05-24 15:22:30 +0100

    Arb.py, Calculate.outcome

---
+++ models/Arb.py
@@ -0,0 +1,32 @@
+import models.Calculate
+import models.CcxtRest
+
+class Arb:
+  def __init__(self, sequential_trades):
+    self.sequential_trades = sequential_trades
+    self.outcomes = []
+    self.arbRate = self.from_sequence()
+
+  def from_sequence(self, start_amount = 1000):
+    first_trade = self.sequential_trades[0]
+    if (first_trade['side'] == 'buy'):
+      first_price = self.sequential_trades[0]['ask']
+    else:
+      first_price= self.sequential_trades[0]['bid']
+    first_outcome = Calculate.outcome(start_amount, first_outcome, first_trade)
+    self.outcomes.append(first_outcome)
+
+    for i, trade in enumerate(self.sequential_trades):
+      if i > 0:
+        previous_end_amount = self.outcomes[i - 1]
+
+        if trade['side'] == 'buy':
+          price = trade['ask']
+        elif trade['side'] == 'ask':
+          price = trade['bid']
+
+        outcome = Calculate.outcome(previous_end_amount, price, trade)
+        self.outcomes.append(outcome)
+
+        arb_rate = Calculate.relative_difference(start_amount, self.outcomes[len(self.outcomes) - 1])
+        return arb_rate
--- models/Calculate.py
+++ models/Calculate.py
@@ -3,3 +3,10 @@ class Calculate:
   def relative_difference(priceA, priceB):
     relative_difference = round(((priceA - priceB) / max(priceA, priceB) * 100), 2)
     return relative_difference
+
+  @staticmethod
+  def outcome(from_amount, trade_price, trade):
+    if trade['to'] == trade['quote']:
+      return from_amount * trade_price
+    else:
+      return from_amount / trade_price
